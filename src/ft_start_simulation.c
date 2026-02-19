a144116b8b6ca9ee4620f4cb57184825a016f02b
Lucca Cruz <lucca1609.ls@gmail.com>
2026-02-19 11:50:26 +0000

    fix: change the thread creation order to avoid deadlocks.

--- src/ft_start_simulation.c
+++ src/ft_start_simulation.c
@@ -6,16 +6,37 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 16:33:54 by lseabra-          #+#    #+#             */
-/*   Updated: 2026/02/18 16:22:26 by lseabra-         ###   ########.fr       */
+/*   Updated: 2026/02/19 11:43:19 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+#include <pthread.h>
+#include <unistd.h>
 
-static t_result	ft_create_threads(t_simulation *sim)
+static t_result ft_create_threads_step(t_simulation *sim, int start, int step)
 {
 	t_philosopher	*philo;
 	int				i;
+
+	i = start;
+	while (i < sim->philo_count)
+	{
+		philo = sim->philos + i;
+		if (pthread_create(&philo->thread, NULL,
+				ft_routine, (void *)philo) != SUCCESS)
+		{
+			ft_put_error(&sim->print_lock, "ft_create_threads_step()",
+				ERR_THREAD_CREATE);
+			return (FAILURE);
+		}
+		i += step;
+	}
+	return (SUCCESS);
+}
+
+static t_result	ft_create_threads(t_simulation *sim)
+{
 
 	if (pthread_create(&sim->monitoring_thread,
 			NULL, ft_monitoring_routine, sim) != SUCCESS)
@@ -24,19 +45,11 @@ static t_result	ft_create_threads(t_simulation *sim)
 			ERR_THREAD_CREATE);
 		return (FAILURE);
 	}
-	i = 0;
-	while (i < sim->philo_count)
-	{
-		philo = sim->philosophers + i;
-		if (pthread_create(&philo->thread, NULL,
-				ft_routine, (void *)philo) != SUCCESS)
-		{
-			ft_put_error(&sim->print_lock, "ft_create_threads()",
-				ERR_THREAD_CREATE);
-			return (FAILURE);
-		}
-		i++;
-	}
+	if (ft_create_threads_step(sim, 0, 2) != SUCCESS)
+		return (FAILURE);
+	usleep(1000);
+	if (ft_create_threads_step(sim, 1, 2) != SUCCESS)
+		return (FAILURE);
 	return (SUCCESS);
 }
 
@@ -48,7 +61,7 @@ static t_result	ft_join_threads(t_simulation *sim)
 	i = 0;
 	while (i < sim->philo_count)
 	{
-		philo = sim->philosophers + i;
+		philo = sim->philos + i;
 		if (pthread_join(philo->thread, NULL) != SUCCESS)
 		{
 			ft_put_error(&sim->print_lock, NULL, ERR_THREAD_JOIN);
