f45b0960f065d256412bebd33f55b1374a53ac6f
Lucca Cruz <lucca1609.ls@gmail.com>
2026-02-24 15:33:02 +0000

    fix: norm errors.

--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@
 /*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 16:39:46 by lseabra-          #+#    #+#             */
-/*   Updated: 2026/02/23 21:06:20 by lseabra-         ###   ########.fr       */
+/*   Updated: 2026/02/24 15:32:48 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 #include <stdlib.h>
 #include <string.h>
+
 int	main(int argc, char **argv)
 {
 	t_simulation	sim;
@@ -23,5 +24,5 @@ int	main(int argc, char **argv)
 	if (ft_start_simulation(&sim) == FAILURE)
 		return (EXIT_FAILURE);
 	ft_cleanup_simulation(&sim);
-		return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
